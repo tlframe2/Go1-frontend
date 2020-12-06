b71484b6db8aa0b05eda985ff101b44c5489c9b7
Tyrell <tlframe2@gmail.com>
2020-12-06 17:36:57 -0500

    added more comments

--- src/utilities/dateFormatter.js
+++ src/utilities/dateFormatter.js
@@ -4,11 +4,8 @@ const splitDatetime = datetimeStr => datetimeStr.split('T');
 const splitTimezone = timeStr => timeStr.split('.');
 const splitDate = dateStr => dateStr.split('-');
 
+// Formats date and time into a more readable format. Ex: 2021-03-24 becomes Mar 24, 2021
 const dateFormatter = datetimeStr => {
-  // if (!datetimeStr) {
-  //   return "";
-  // }
-
   let [date, timeAndTimezone] = splitDatetime(datetimeStr);
   let [time, timezone] = splitTimezone(timeAndTimezone)
   let [year, month, day] = splitDate(date);
