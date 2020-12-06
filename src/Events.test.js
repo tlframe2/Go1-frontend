2d21edc04b2d8eee0c0944454ab8cff6a90aff57
Tyrell <tlframe2@gmail.com>
2020-12-06 00:51:49 -0500

    added and tested axios call to get events

---
+++ src/Events.test.js
@@ -0,0 +1,181 @@
+import React from 'react';
+import { render, waitFor, cleanup, screen } from '@testing-library/react';
+import App from './App';
+import Event from './components/Event';
+import mockedAxios from 'axios';
+
+afterEach(cleanup);
+
+test('mocks axios request and renders events', async () => {
+  const data = {
+    data: [
+      {
+        Title: "Infection Prevention and Control (Australia)",
+        Time: "2021-03-22T02:30:00.000Z",
+        Image: "http://example.com/image.png",
+        Location: {
+          City: "Brisbane",
+          State: "Queensland",
+          Country: "Australia",
+        }
+      },
+      {
+        Title: "Wellbeing First - Mindfulness",
+        Time: "2021-03-24T02:30:00.000Z",
+        Image: "http://example.com/image.png",
+        Location: {
+          City: "Cairns",
+          State: "Queensland",
+          Country: "Australia",
+        },
+        AvailableSeats: [
+          {
+            id: "W25"
+          },
+          {
+            id: "B29"
+          }
+        ]
+      },
+      {
+        Title: "10 Minutes Managing Stress",
+        Time: "2021-03-24T02:30:00.000Z",
+        Image: "http://example.com/image.png",
+        Location: {
+          City: "Gold Coast",
+          State: "Queensland",
+          Country: "Australia",
+        },
+        AvailableSeats: [
+          {
+            id: "W25"
+          },
+          {
+            id: "B29"
+          }
+        ]
+      },
+      {
+        Title: "10 Minute Pandemic Awareness",
+        Time: "2021-03-24T02:30:00.000Z",
+        Image: "http://example.com/image.png",
+        Location: {
+          City: "Gold Coast",
+          State: "Queensland",
+          Country: "Australia",
+        },
+        AvailableSeats: [
+          {
+            id: "W25"
+          },
+          {
+            id: "B29"
+          }
+        ]
+      },
+      {
+        Title: "Morning Yoga Kickstart",
+        Time: "2021-03-24T02:30:00.000Z",
+        Image: "http://example.com/image.png",
+        Location: {
+          City: "Gold Coast",
+          State: "Queensland",
+          Country: "Australia",
+        },
+        AvailableSeats: [
+          {
+            id: "W25"
+          },
+          {
+            id: "B29"
+          }
+        ]
+      },
+      {
+        Title: "Hand Hygiene in the Workplace",
+        Time: "2021-03-24T02:30:00.000Z",
+        Image: "http://example.com/image.png",
+        Location: {
+          City: "Gold Coast",
+          State: "Queensland",
+          Country: "Australia",
+        },
+        AvailableSeats: [
+          {
+            id: "W25"
+          },
+          {
+            id: "B29"
+          }
+        ]
+      },
+      {
+        Title: "Equal Employment Opportunity",
+        Time: "2021-03-24T02:30:00.000Z",
+        Image: "http://example.com/image.png",
+        Location: {
+          City: "Gold Coast",
+          State: "Queensland",
+          Country: "Australia",
+        },
+        AvailableSeats: [
+          {
+            id: "W25"
+          },
+          {
+            id: "B29"
+          }
+        ]
+      },
+      {
+        Title: "First Aid - Basics",
+        Time: "2021-03-24T02:30:00.000Z",
+        Image: "http://example.com/image.png",
+        Location: {
+          City: "Gold Coast",
+          State: "Queensland",
+          Country: "Australia",
+        },
+        AvailableSeats: [
+          {
+            id: "W25"
+          },
+          {
+            id: "B29"
+          }
+        ]
+      },
+    ]
+  };
+
+  mockedAxios.get.mockResolvedValueOnce(data);
+
+  render(<App />);
+
+  await waitFor(() => {
+    expect(screen.getByText("Infection Prevention and Control (Australia)")).toBeInTheDocument();
+    expect(screen.getByText("Wellbeing First - Mindfulness")).toBeInTheDocument();
+  });
+
+});
+
+test('renders event properties', () => {
+  render(<Event title={"Infection Prevention and Control (Australia)"} time={"2021-03-22T02:30:00.000Z"} />);
+  expect(screen.getByText("Infection Prevention and Control (Australia)")).toBeInTheDocument();
+  expect(screen.getByText("2021-03-22T02:30:00.000Z")).toBeInTheDocument();
+});
+
+// test('app renders correctly', () => {
+//   const history = createMemoryHistory();
+
+//   render(
+//     <Router history={history}>
+//       <App />
+//     </Router>
+//   );
+
+//   expect(screen.getByText("Infection Prevention and Control (Australia)")).toBeInTheDocument();
+//   expect(screen.getByText("2021-03-22T02:30:00.000Z")).toBeInTheDocument();
+//   expect(screen.getByText("Wellbeing First - Mindfulness")).toBeInTheDocument();
+//   expect(screen.getByText("2021-03-24T02:30:00.000Z")).toBeInTheDocument();
+// })
--- src/components/EventsOverview.js
+++ src/components/EventsOverview.js
@@ -1,46 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Event from './Event';
+import axios from 'axios';
 
 const EventsOverview = () => {
-  const [events, setEvents] = useState([
-    {
-      Title: "Infection Prevention and Control (Australia)",
-      Time: "2021-03-22T02:30:00.000Z",
-      Image: "http://example.com/image.png",
-      Location: {
-        City: "Brisbane",
-        State: "Queensland",
-        Country: "Australia",
-      }
-    },
-    {
-      Title: "Wellbeing First - Mindfulness",
-      Time: "2021-03-24T02:30:00.000Z",
-      Image: "http://example.com/image.png",
-      Location: {
-        City: "Cairns",
-        State: "Queensland",
-        Country: "Australia",
-      },
-      AvailableSeats: [
-        {
-          id: "W25"
-        },
-        {
-          id: "B29"
-        }
-      ]
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const getEvents = async () => {
+      const { data } = await axios.get('http://localhost:5000/api/events');
+      setEvents(data);
     }
-  ]);
+
+    getEvents();
+  }, []);
 
   return (
     <>
       {
-        events.map((event, index) => (
-          <div>
-            <Event title={event.Title} time={event.Time} image={event.Image} eventLocation={event.Location} seats={event.AvailableSeats} key={index} />
-          </div>
-        ))
+        events.length === 0 ? (
+          <h1>Loading...</h1>
+        ) : (
+          events.map((event, index) => (
+            <div>
+              <Event title={event.Title} time={event.Time} image={event.Image} eventLocation={event.Location} seats={event.AvailableSeats} key={index} data-testid="event" />
+            </div>
+          ))
+        )
       }
     </>
   );
